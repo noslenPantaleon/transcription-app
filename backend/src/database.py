7b055b32e72019c6638a52c5ed250434b26987f0
noslenpantaleon <pantaleon.noslen@gmail.com>
2025-02-26 22:00:08 -0300

    add config settings in database

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,12 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgrepass@localhost:5432/transcription'
-
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
